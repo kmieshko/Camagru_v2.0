9d31c279c31cd4eaaa0b8190c0df575bb3fa287f
Kyrylo MIESHKOV <meshkov.kirill@gmail.com>
2019-03-06 20:03:47 +0200

    config/setup.php added

--- config/setup.php
+++ config/setup.php
@@ -1,1 +1,7 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "5636763";
+$conn = mysqli_connect($servername, $username, $password);
+$sql = "CREATE DATABASE `camagru`";
+mysqli_query($conn, $sql);
